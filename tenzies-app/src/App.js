11c153466b4ca8ba391d14f11d935ec1119c01de
KoliosMarios <koliosmarios@gmail.com>
2022-06-11 19:30:38 +0300

    styling held dice

--- tenzies-app/src/App.js
+++ tenzies-app/src/App.js
@@ -2,26 +2,38 @@ import React from "react";
 import Die from "./Die";
 
 function App() {
+  //create the state dice array
   const [dice, setDice] = React.useState(allNewDice());
 
   function allNewDice() {
+    //creating a new array to put the random numbers
+    //then we changed to an array of objects so we can have the isHeld property
     let newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
     }
     return newDice;
   }
 
+  //we change the dice array by calling the allNewDice function again
   function rollDice() {
     setDice(allNewDice);
   }
 
-  const diceElements = dice.map((die) => <Die value={die} />);
+  //map through the dice array to display the numbers
+  const diceElements = dice.map((die, index) => (
+    <Die key={index} value={die.value} isHeld={die.isHeld} />
+  ));
 
   return (
     <div className="main">
       <div className="dice-container">{diceElements}</div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </div>
   );
 }
--- tenzies-app/src/Die.js
+++ tenzies-app/src/Die.js
@@ -1,9 +1,13 @@
-function Die(props){
-    return (
-        <div className="die-face">
-            <h2 className="die-num">{props.value}</h2>
-        </div>
-    )
+function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59e391" : "white",
+  };
+
+  return (
+    <div className="die-face" style={styles}>
+      <h2 className="die-num">{props.value}</h2>
+    </div>
+  );
 }
 
-export default Die
+export default Die;
